6ad9c487d2ca80e0798b905df4f7ff93180b2ae3
abhinayrimil <abhinayrrimil@gmail.com>
2024-11-08 23:49:33 +0530

    iam-policy.tf  rectified

--- iac/iam-policy.tf
+++ iac/iam-policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "iam-policy" {
     role = aws_iam_role.iam-role.name
-    policy_arn = "arn:aws:iam::aws:policy/dministratorAccess"  // Not recommended for live-production environments.
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"  // Not recommended for live-production environments.
   
 }
